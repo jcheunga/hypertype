4ed49286e01cfba141a8bd30d2ce01312f5e42f4
jcheunga <jasoncheunga@gmail.com>
2017-04-23 00:18:24 +1000

    Fix feathers socket connect

--- src/feathers/index.js
+++ src/feathers/index.js
@@ -1,14 +1,17 @@
-import io from 'socket.io-client';
-import feathers from 'feathers/client'
+// import io from 'socket.io-client';
+const io = require("socket.io-client/dist/socket.io");
+import feathers from 'feathers/client';
 import hooks from 'feathers-hooks';
-import socketio from 'feathers-socketio/client'
+import socketio from 'feathers-socketio/client';
 import authentication from 'feathers-authentication-client';
+
+import { AsyncStorage } from 'react-native';
 
 import reduxifyServices, { getServicesStatus } from 'feathers-reduxify-services';
 import reduxifyAuthentication from 'feathers-reduxify-authentication';
 
-const API_URL = 'http://localhost:9090';
-const options = {transports: ['websocket'], pingTimeout: 3000, pingInterval: 5000};
+const API_URL = 'http://10.0.2.2:3000';
+const options = {transports: ['websocket'], pingTimeout: 3000, pingInterval: 5000, jsonp: false};
 const socket = io(API_URL, options);
 
 const app = feathers()
