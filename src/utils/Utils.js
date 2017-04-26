37950412f80f604e884f5291254ed105561fe75e
jcheunga <jasoncheunga@gmail.com>
2017-04-27 00:46:41 +1000

    condense into utils

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -3,6 +3,19 @@ export function getTodayFormatted() {
   return new Date().toISOString().slice(0,10).split('-').join('');
 }
 
-export function getRamdonNumberBetweenRange(max, min) {
+export function getRandomNumberBetweenRange(max, min) {
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+export function createRandomGameId () {
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var letterPart = alphabet[getRandomNumberBetweenRange(0, 26)] + alphabet[getRandomNumberBetweenRange(0, 26)];
+  var numberPart = getRandomNumberBetweenRange(1000, 9999).toString();
+  var totalGameId = letterPart + numberPart;
+  return totalGameId
+}
+
+export function getQuoteToType () {
+  const quoteArr = ["a", "b", "c"];
+  return quoteArr[getRandomNumberBetweenRange(0, 3)];
 }
