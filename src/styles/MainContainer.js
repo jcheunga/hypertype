10b62f3c22b21adaf83f0fdc00baf5ebc17141fd
Jason Cheung <jason@dokio.co>
2017-06-14 12:47:07 +1000

    add platform specific for maincontainer

--- src/styles/MainContainer.js
+++ src/styles/MainContainer.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 
 const MainContainer = styled.ScrollView`
+  padding-top: ${(Platform.OS === 'ios') ? '20' : '0'};
   flex: 1;
   background-color: ${props => {
     if (props.userOn) {
